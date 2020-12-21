6ebc66499db54df68c7736844a9f3c5016d6d479
Dmitry Ivanov <di.ivanov2018@gmail.com>
2020-12-22 00:51:00 +0300

    Run lint:fix on project

--- src/example.test.js
+++ src/example.test.js
@@ -1,7 +1,7 @@
-import { sum } from './example';
+import { sum } from "./example";
 
-describe('Simple test for example.js', () => {
-  it('return sum of params', () => {
+describe("Simple test for example.js", () => {
+  it("return sum of params", () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
