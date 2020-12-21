678539a5ccd6b5a2a174aeb391011c5be1801c33
Dmitry Ivanov <di.ivanov2018@gmail.com>
2020-12-22 00:32:41 +0300

    Tweak some eslint rules

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    presets: [
-        [
-            "@babel/preset-env",
-            {
-                targets: 'defaults'
-            },
-        ],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: 'defaults',
+      },
     ],
+  ],
 };
--- src/example.test.js
+++ src/example.test.js
@@ -1,7 +1,7 @@
-import {sum} from './example';
+import { sum } from './example';
 
 describe('Simple test for example.js', () => {
-    it('return sum of params', function () {
-        expect(sum(1, 2)).toBe(3);
-    });
-})
+  it('return sum of params', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
