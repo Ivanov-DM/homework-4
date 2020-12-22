0c0b56d00ed7836634ff80910eaa43b9378ff8fb
Dmitry Ivanov <di.ivanov2018@gmail.com>
2020-12-23 01:45:49 +0300

    Create function createForm in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-import { sum } from "./example";
+function createForm(el) {
+  const inputForm = document.createElement("form");
 
-function component() {
-  const element = document.createElement("div");
-  element.innerHTML = `1 + 2 = ${sum(1, 2)}`;
-  return element;
+  const inputField = document.createElement("input");
+  inputField.id = "userInput";
+  inputField.placeholder = "Type city and press enter!";
+  inputField.required = true;
+  inputField.autofocus = true;
+
+  const btn = document.createElement("button");
+  btn.innerText = "Get weather";
+
+  const weatherBlock = document.createElement("div");
+  weatherBlock.id = "weatherBlock";
+
+  const historyList = document.createElement("div");
+  historyList.id = "historyList";
+
+  const historyListHeader = document.createElement("h2");
+  historyListHeader.innerText = "You have watched the weather in:";
+
+  const cityMap = document.createElement("img");
+  cityMap.id = "cityMap";
+
+  inputForm.appendChild(inputField);
+  inputForm.appendChild(btn);
+
+  el.appendChild(inputForm);
+  el.appendChild(weatherBlock);
+  el.appendChild(cityMap);
+  el.appendChild(historyListHeader);
+  el.appendChild(historyList);
 }
 
-document.body.appendChild(component());
+const container = document.querySelector("#container");
+createForm(container);
