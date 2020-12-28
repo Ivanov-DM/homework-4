bd6d1605b32fc9d687abaf00551a2436ee3e0395
Dmitry Ivanov <di.ivanov2018@gmail.com>
2020-12-28 12:58:23 +0300

    Uninstall node-fetch

--- src/index.js
+++ src/index.js
@@ -1,37 +1,35 @@
 export function createForm(el) {
-  if (el !== null) {
-    const inputForm = document.createElement("form");
+  const inputForm = document.createElement("form");
 
-    const inputField = document.createElement("input");
-    inputField.id = "userInput";
-    inputField.placeholder = "Type city and press enter!";
-    inputField.required = true;
-    inputField.autofocus = true;
+  const inputField = document.createElement("input");
+  inputField.id = "userInput";
+  inputField.placeholder = "Type city and press enter!";
+  inputField.required = true;
+  inputField.autofocus = true;
 
-    const btn = document.createElement("button");
-    btn.innerText = "Get weather";
+  const btn = document.createElement("button");
+  btn.innerText = "Get weather";
 
-    const weatherBlock = document.createElement("div");
-    weatherBlock.id = "weatherBlock";
+  const weatherBlock = document.createElement("div");
+  weatherBlock.id = "weatherBlock";
 
-    const historyList = document.createElement("div");
-    historyList.id = "historyList";
+  const historyList = document.createElement("div");
+  historyList.id = "historyList";
 
-    const historyListHeader = document.createElement("h2");
-    historyListHeader.innerText = "You have watched the weather in:";
+  const historyListHeader = document.createElement("h2");
+  historyListHeader.innerText = "You have watched the weather in:";
 
-    const cityMap = document.createElement("img");
-    cityMap.id = "cityMap";
+  const cityMap = document.createElement("img");
+  cityMap.id = "cityMap";
 
-    inputForm.appendChild(inputField);
-    inputForm.appendChild(btn);
+  inputForm.appendChild(inputField);
+  inputForm.appendChild(btn);
 
-    el.appendChild(inputForm);
-    el.appendChild(weatherBlock);
-    el.appendChild(cityMap);
-    el.appendChild(historyListHeader);
-    el.appendChild(historyList);
-  }
+  el.appendChild(inputForm);
+  el.appendChild(weatherBlock);
+  el.appendChild(cityMap);
+  el.appendChild(historyListHeader);
+  el.appendChild(historyList);
 }
 
 export function getCurrentCityTitle() {
